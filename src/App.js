5e01d7f52a6b699a5b7311f758abd8edb8cdf5cb
Muhammad Sannan Umer <sannanumer4@gmail.com>
2023-06-03 17:48:21 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef} from 'react';
+import Todos from './Todos';
+import { v4 as uuid } from "uuid";
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  
+
+  const handleRef = useRef();
+
+  const handleOnClick = () =>{
+    const name = handleRef.current.value;
+    if(name==="") return
+    setTodos(prevTodos =>{
+      return [...prevTodos, {id:uuid(), name:name}]
+    });
+    handleRef.current.value = "";
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Todo App</h1>
+      <input type="text" name="todo" ref={handleRef}/>
+      <button type="button" onClick={handleOnClick}>Add todo</button>
+
+      <hr style={{marginTop: '20px'}}/>
+
+      <h1>Tasks:</h1>
+
+      <Todos todos={todos}/>
+    </>
   );
 }
 
---
+++ src/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Todo({todo}) {
+  return (
+    
+        <h2>{todo.name}</h2>
+    
+  )
+}
